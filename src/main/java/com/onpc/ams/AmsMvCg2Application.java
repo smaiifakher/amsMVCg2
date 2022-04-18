6d9114cd205b273167e6283eeb47a010da426190
smaii <smaiifakher1994@gmail.com>
2022-04-18 10:51:50 +0100

    Initial commit

---
+++ src/main/java/com/onpc/ams/AmsMvCg2Application.java
@@ -0,0 +1,13 @@
+package com.onpc.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsMvCg2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmsMvCg2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/onpc/ams/AmsMvCg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpc.ams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsMvCg2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
